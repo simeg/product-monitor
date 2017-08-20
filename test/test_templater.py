0e93252bc2c2c191f0192470d9788df130dcdc6f
Simon Egersand <s.egersand@gmail.com>
2017-08-20 20:59:30 +0200

    Refactor tests for improved clarity

--- test/test_templater.py
+++ test/test_templater.py
@@ -10,45 +10,51 @@ class TestCases(unittest.TestCase):
         assert templater._build_subject() is not None and \
                len(templater._build_subject()) > 1
 
-    def test_build_event_row_product_unavailable(self):
+    def test_build_row_product_unavailable(self):
         event = {'type': 'PRODUCT_UNAVAILABLE', 'url': 'arbitrary-url'}
 
-        row = templater._build_event_row(event)
+        actual_row = templater._build_row(event)
 
-        assert row == '<p><a href="arbitrary-url">En bevakad produkt</a> ' \
-                      'har tagits bort ur sortimentet eller flyttats och ' \
-                      'lanken fungerar inte</p>'
+        expected_row = '<p><a href="arbitrary-url">En bevakad produkt</a> ' \
+                       'har tagits bort ur sortimentet eller flyttats och ' \
+                       'lanken fungerar inte</p>'
 
-    def test_build_event_row_product_price_lower(self):
+        assert actual_row == expected_row
+
+    def test_build_row_product_price_lower(self):
         event = {'type': 'PRICE_LOWER', 'url': 'arbitrary-url',
                  'new_price': '50', 'old_price': '100'}
 
-        row = templater._build_event_row(event)
+        actual_row = templater._build_row(event)
 
-        assert row == '<p>Priset pa <a href="arbitrary-url">en bevakad ' \
-                      'produkt</a> har <strong>sankts</strong> fran 100kr ' \
-                      'till 50kr (med 50kr skillnad)</p>'
+        expected_row = '<p>Priset pa <a href="arbitrary-url">en bevakad ' \
+                       'produkt</a> har <strong>sankts</strong> fran 100kr ' \
+                       'till 50kr (med 50kr skillnad)</p>'
 
-    def test_build_event_row_product_price_higher(self):
+        assert actual_row == expected_row
+
+    def test_build_row_product_price_higher(self):
         event = {'type': 'PRICE_HIGHER', 'url': 'arbitrary-url',
                  'new_price': '450', 'old_price': '100'}
 
-        row = templater._build_event_row(event)
+        expected_row = templater._build_row(event)
 
-        assert row == '<p>Priset pa <a href="arbitrary-url">en bevakad ' \
-                      'produkt</a> har <strong>hojts</strong> fran 100kr ' \
-                      'till 450kr (med 350kr skillnad)</p>'
+        actual_row = '<p>Priset pa <a href="arbitrary-url">en bevakad ' \
+                     'produkt</a> har <strong>hojts</strong> fran 100kr ' \
+                     'till 450kr (med 350kr skillnad)</p>'
 
-    def test_build_event_row_product_unknown_event_type(self):
+        assert expected_row == actual_row
+
+    def test_build_row_product_unknown_event_type(self):
         event = {'type': 'NON_EXISTING_TYPE'}
 
-        self.assertRaises(ValueError, templater._build_event_row, event)
+        self.assertRaises(ValueError, templater._build_row, event)
 
     def test_build_with_single_event(self):
         events = [{'type': 'PRICE_HIGHER', 'url': 'arbitrary-url',
                    'new_price': '450', 'old_price': '100'}]
 
-        template = templater.build(events)
+        actual_template = templater.build(events)
 
         expected_template = {'subject': 'En eller flera av dina bevakade '
                                         'produkter har uppdaterats',
@@ -57,7 +63,7 @@ class TestCases(unittest.TestCase):
                                      'hojts</strong> fran 100kr till 450kr '
                                      '(med 350kr skillnad)</p>'}
 
-        assert template == expected_template
+        assert actual_template == expected_template
 
     def test_build_with_multiple_event(self):
         events = [{'type': 'PRICE_HIGHER', 'url': 'arbitrary-url',
@@ -65,7 +71,7 @@ class TestCases(unittest.TestCase):
                   {'type': 'PRICE_LOWER', 'url': 'arbitrary-url',
                    'new_price': '50', 'old_price': '100'}]
 
-        template = templater.build(events)
+        actual_template = templater.build(events)
 
         expected_template = {'subject': 'En eller flera av dina bevakade '
                                         'produkter har uppdaterats',
@@ -78,4 +84,4 @@ class TestCases(unittest.TestCase):
                                      'sankts</strong> fran 100kr '
                                      'till 50kr (med 50kr skillnad)</p>'}
 
-        assert template == expected_template
+        assert actual_template == expected_template
